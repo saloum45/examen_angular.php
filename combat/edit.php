557a6b33653a62097a3f1a1d24105a1793fa7ba9
saloum45 <saloumfall45@gmail.com>
2024-01-21 20:55:26 +0000

    in progress

--- combat/edit.php
+++ combat/edit.php
@@ -1,24 +1,26 @@
 <?php
+
 use Taf\TafAuth;
 use Taf\TableQuery;
+
 try {
     require './config.php';
     require '../TableQuery.php';
     require '../taf_auth/TafAuth.php';
     $taf_auth = new TafAuth();
     // toutes les actions nécéssitent une authentification
-    $auth_reponse=$taf_auth->check_auth($reponse);
+    $auth_reponse = $taf_auth->check_auth($reponse);
     if ($auth_reponse["status"] == false) {
         echo json_encode($auth_reponse);
         die;
     }
-    
-    $table_query=new TableQuery($table_name);
-   /* 
+
+    $table_query = new TableQuery($table_name);
+    /* 
         $params
         contient tous les parametres envoyés par la methode POST
      */
-    extract($_POST);
+    extract($params);
     $query = "UPDATE  `combat` SET  `titre`='$titre',`description_combat`='$description', `date_combat`='$date_combat',`id_lutteur1`='$id_lutteur1',`id_lutteur2`='$id_lutteur2',`resultat`='$resultat' WHERE id='$id'";
     // $reponse["query"]=$query;
     if ($taf_config->get_db()->exec($query)) {
@@ -31,11 +33,9 @@ try {
     }
     echo json_encode($reponse);
 } catch (\Throwable $th) {
-    
+
     $reponse["status"] = false;
     $reponse["erreur"] = $th->getMessage();
 
     echo json_encode($reponse);
 }
-
-?>
